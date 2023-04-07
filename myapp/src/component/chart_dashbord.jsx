63b48caded8f37ac17e8eb2487bcf326d87157a4
Nikhil-81 <nikhilwalwatkar81@gmail.com>
2023-04-07 15:38:14 +0530

    chart_added_with_dynamic_function

---
+++ myapp/src/component/chart_dashbord.jsx
@@ -0,0 +1,119 @@
+import { Line } from "react-chartjs-2";
+
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Flex, Select, Text } from "@chakra-ui/react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  responsive: true,
+
+  scales: {
+    y: {
+      ticks: {
+        callback: function (value, index, ticks) {
+          return "%" + value;
+          // console.log(ticks)
+        },
+      },
+    },
+  },
+};
+
+let labels = [
+  "5k",
+  "10k",
+  "15k",
+  "20k",
+  "25k",
+  "30k",
+  "35k",
+  "40k",
+  "45k",
+  "50k",
+  "55k",
+  "60k",
+];
+
+let month = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+
+
+
+
+
+export default function Chart_line() {
+    const [data_set,setdata]=useState()
+    const [MONTH,setmonth]=useState("January")
+    function hendleChange(month) {
+        setmonth(month)
+    }
+
+    function changePer(chart_data) {
+        let num =chart_data[0].data
+        let main_data = [];
+        num.map((el) => main_data.push((Number(el) / chart_data[0].max) * 100));
+      console.log(main_data)
+        setdata(main_data)
+      }
+
+    useEffect(()=>{
+        axios.get(`https://fackstore.onrender.com/sales?month=${MONTH}`).then(res=>changePer(res.data)).catch(err=>console.log(err))
+    },[MONTH])
+  return (
+    <div className="chart_cont">
+      <Flex justifyContent="space-between">
+        <Text>Sales Details</Text>
+        <Select
+          placeholder="Month"
+          w="20%"
+          className="Month_select"
+          onChange={(e) => hendleChange(e.target.value)}
+        >
+          {month.map((el) => (
+            <option value={el}>{el}</option>
+          ))}
+        </Select>
+      </Flex>
+      <Line options={options} data={{labels,
+  datasets: [
+    {
+      data: data_set,
+      borderColor: "#4880FF",
+      backgroundColor: "white",
+    },
+  ],}} />;
+    </div>
+  );
+}
--- myapp/src/page/dashbord.jsx
+++ myapp/src/page/dashbord.jsx
@@ -1,12 +1,12 @@
 
-import NavBar from "../component/navbr";
-import Sidebar from "../component/sidebar";
+import Chart_line from "../component/chart_dashbord";
+
 import "../styles/dashbord.css"
 export default function Dashbord(){
     return (
         
       <div style={{background:"#F5F5F5"}} >
-    
+    <Chart_line/>
       </div>
     )
 }
